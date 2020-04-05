b22fd21e44ca9676b84fdd4e529471b2b55d13e0
Jesse J. Wattenbarger <jesse.j.wattenbarger@gmail.com>
2020-04-05 14:36:53 -0400

    Initial commit of working home-manager config on armchair-traveler.

---
+++ home.nix
@@ -0,0 +1,98 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  manual.html.enable = true;
+
+  home.packages = with pkgs; [
+                jq
+                ripgrep
+                antigen
+  ];
+
+
+  programs.git = {
+    package = pkgs.gitAndTools.gitFull;
+    enable = true;
+    userName = "Jesse Wattenbarger";
+    userEmail = "jwatt@broken.watch";
+  };
+
+  programs.command-not-found.enable = true;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    # environment.pathsToLink = [ "/share/zsh" ];
+    autocd = true;
+    history.ignoreSpace = true;
+    initExtra = ''
+source "${pkgs.antigen}/share/antigen/antigen.zsh"
+antigen use oh-my-zsh
+antigen bundle git
+antigen bundle tmux
+# antigen bundle pyenv
+antigen bundle colorize
+antigen bundle colored-man-pages
+antigen bundle fzf
+antigen bundle urbainvaes/fzf-marks
+antigen bundle zdharma/zsh-diff-so-fancy
+antigen bundle wfxr/forgit
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-completions
+# antigen bundle zsh-users/zsh-cdr
+antigen bundle zsh-users/zaw
+antigen bundle command-not-found
+# antigen bundle asdf
+# antigen bundle vi-mode
+# antigen theme robbyrussell
+antigen bundle nojhan/liquidprompt
+antigen bundle spwhitt/nix-zsh-completions.git
+antigen apply
+antigen theme agnoster
+antigen apply
+# antigen theme bhilburn/powerlevel9k powerlevel9k
+    '';
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    defaultCommand = "${pkgs.ripgrep}/bin/rg --files";
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+    defaultCacheTtl = 1800;
+  };
+
+
+  # X setup; untested
+  # xsession = {
+  #   enable = true;
+  #   windowManager.xmonad = {
+  #     enable = true;
+  #     enableContribAndExtras = true;
+  #     # config =
+  #   };
+  # }
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "19.09";
+}
